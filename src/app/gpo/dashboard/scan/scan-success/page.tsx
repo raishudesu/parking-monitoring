3748f13c29684a65ac24ed8092f85849af033c09
raishudesu <bacaltosbaryshnikov@gmail.com>
2024-09-28 16:24:46 +0800

    update scan success page

--- src/app/gpo/dashboard/scan/scan-success/page.tsx
+++ src/app/gpo/dashboard/scan/scan-success/page.tsx
@@ -18,16 +18,16 @@ const ScanSuccess = () => {
   }, [router]);
 
   return (
-    <div className="text-center w-full h-full flex flex-col gap-12 justify-center items-center">
+    <div className="text-center w-full h-full flex flex-col gap-6 items-center mt-12">
+      <BadgeCheck size={200} className="text-green-500" />
       <div className="">
-        <h1 className="scroll-m-20 text-3xl font-extrabold tracking-tight lg:text-4xl">
+        <h1 className="scroll-m-20 text-xl md:text-3xl font-extrabold tracking-tight lg:text-4xl">
           Parking Session Started
         </h1>
         <p className="leading-7 [&:not(:first-child)]:mt-6 text-muted-foreground">
           Remember to end your session on time!
         </p>
       </div>
-      <BadgeCheck size={200} className="text-green-500" />
     </div>
   );
 };
